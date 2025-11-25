9b21b903906ccf55c7bb2073c258198b62429121
516 Hackers <125684516+516hackers@users.noreply.github.com>
2025-11-26 01:30:26 +0500

    Add UserInDB and Business models with validation

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,15 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
 
+
+class UserInDB(BaseModel):
+_id: Optional[str]
+name: str
+email: EmailStr
+hashed_password: str
+
+
+class Business(BaseModel):
+_id: Optional[str]
+user_id: str
+text: str
